aeba56a7908b50676d192f83a8f491c2b69f837e
pippinbarr <pippin.barr@gmail.com>
2026-03-31 12:32:35 -0400

    NESJS version mostly working
    - Had my usual tomfoolery with CSS and trying to make things all lovely and responsive before settling for "good enough"
    - I suppose you can't figure out good enough without seeing bad bad bad?
    - Anyway it's good enough

--- js/main.js
+++ js/main.js
@@ -1,14 +1,21 @@
-const browser = new jsnes.Browser({
-    container: document.getElementById("nes"),
-    onError: function (e) {
-        console.error(e);
-    },
-});
+const clickToStart = document.getElementById("clicktostart");
+clickToStart.addEventListener("click", startEmulator);
 
-jsnes.Browser.loadROMFromURL("rom/play-it-by-trust.nes", function (err, data) {
-    if (err) {
-        console.error(err);
-        return;
-    }
-    browser.loadROM(data);
-});
+function startEmulator() {
+    clickToStart.style.display = "none";
+
+    const browser = new jsnes.Browser({
+        container: document.getElementById("nes"),
+        onError: function (e) {
+            console.error(e);
+        },
+    });
+
+    jsnes.Browser.loadROMFromURL("rom/play-it-by-trust.nes", function (err, data) {
+        if (err) {
+            console.error(err);
+            return;
+        }
+        browser.loadROM(data);
+    });
+}
